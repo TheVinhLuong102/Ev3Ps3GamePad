66d7734b17d4758e9d77fb627859323ca4cfdbd4
Anton Vanhoucke <Anton@fabrique.nl>
2017-12-02 12:23:32 +0100

    better shooting

--- run_ballsnatcher.py
+++ run_ballsnatcher.py
@@ -40,7 +40,6 @@ for device in devices:
     if device.name == 'PLAYSTATION(R)3 Controller':
         ps3dev = device.fn
 
-
 gamepad = evdev.InputDevice(ps3dev)
 
 
@@ -53,39 +52,52 @@ medium_motor_speed = -300
 shooting = 0
 running = True
 
-class MotorThread(threading.Thread):
+class ShooterThread(threading.Thread):
     def __init__(self):
-        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
-        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
         self.medium_motor = ev3.MediumMotor(ev3.OUTPUT_A)
         self.touch_sensor = ev3.TouchSensor(ev3.INPUT_1)
         threading.Thread.__init__(self)
 
     def run(self):
-        print("Engines running!")
+        print("Shooter running!")
         self.medium_motor.run_forever(speed_sp=300)
         while not self.touch_sensor.is_pressed:
             time.sleep(0.1)
         self.medium_motor.run_to_rel_pos(position_sp=180, speed_sp=300)
         while running:
-            self.left_motor.run_forever(speed_sp=clamp(fwd_speed + side_speed//2,(-700,700)))
-            self.right_motor.run_forever(speed_sp=clamp(fwd_speed - side_speed//2,(-700,700)))
             if shooting:
+                self.medium_motor.run_forever(speed_sp=700)
                 while not self.touch_sensor.is_pressed:
                     time.sleep(0.1)
                 self.medium_motor.run_to_rel_pos(position_sp=180, speed_sp=700)
             else:
                 self.medium_motor.run_forever(speed_sp=medium_motor_speed)
 
+        self.medium_motor.stop()
+
+class MotorThread(threading.Thread):
+    def __init__(self):
+        self.left_motor = ev3.LargeMotor(ev3.OUTPUT_B)
+        self.right_motor = ev3.LargeMotor(ev3.OUTPUT_C)
+        threading.Thread.__init__(self)
+
+    def run(self):
+        print("Engines running!")
+        while running:
+            self.left_motor.run_forever(speed_sp=clamp(fwd_speed + side_speed//2, (-700,700)))
+            self.right_motor.run_forever(speed_sp=clamp(fwd_speed - side_speed//2, (-700,700)))
+
         self.left_motor.stop()
         self.right_motor.stop()
-        self.medium_motor.stop()
 
 
 if __name__ == "__main__":
     motor_thread = MotorThread()
     motor_thread.setDaemon(True)
     motor_thread.start()
+    shooter_thread = ShooterThread()
+    shooter_thread.setDaemon(True)
+    shooter_thread.start()
 
     for event in gamepad.read_loop(): #this loops infinitely
         if event.type == 3: #A stick is moved
